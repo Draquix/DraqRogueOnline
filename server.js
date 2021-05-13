e2d21239ccc3d8266b818b71711813c69dcff4fa
Draquix <Draquix@yahoo.com>
2021-05-13 10:47:05 -0500

    have data drawn, working on emiting it properly

--- server.js
+++ server.js
@@ -12,14 +12,86 @@ const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, '/static')));
 
+let SOCKET_LIST = {};
+let PLAYER_LIST = {};
+let MapBox = [];
+
+
 io.on('connection', socket => {
     console.log('Some client connected...');
 
+    socket.on('login', user => {
+        console.log('player login: ', user);
+        player = new Player(user.username, user.passphrase, socket.id);
+        PLAYER_LIST[socket.id] = player;
+    });
     socket.on('chat', message => {
         console.log('message from client: ', message);
         io.emit('chat', {message, id: socket.id});
     });
 });
+
+setInterval(function() {
+    let pack = [];
+    for (var i in MAP_LIST){
+        pack.push(MapBox[i]);
+        for()
+        for (var i in SOCKET_LIST){
+            let socket = SOCKET_LIST[i];
+            socket.emit('draw map', pack);
+        };
+    }
+
+   
+}, 256);
+
+function Player (name, passphrase, id){
+    this.username = name;
+    this.passphrase = passphrase;
+    this.id = id;
+    this.stats = {
+        str:1,dex:1,def:1,mHp:10,hp:10,coin:100
+    };
+    this.map = 0;
+}
+const map0 = {
+    map:[
+        ['#','#','#','#','#','#','#','#','#','#','#','#','#'],
+        ['#','.',',','.','.',',','.','.',',','.','.',',','#'],
+        ['#','.',',','.','.',',','.','.',',','.','.',',','#'],
+        ['#','.','.','.','.','.','.','.','.','.','.','.','#'],
+        ['#','.','.','P','.','.','.','.','.','#','+','#','#'],
+        ['#',',','.','.',',','.','P',',','.','#'],
+        ['#',',','.','.',',','.','%',',','.','#'],
+        ['#','#','#','#','#','#','#','#','#','#'],
+        ],
+    NPC: ["red","green"],
+    Wall: 'brown',
+    floorDots: 'grey',
+    floorSpots: 'blue'
+};
+const map1 = {
+    map:[
+        ['#','#','#','#','#','#','_','_','_','_','_','_','#','#','#','#','#','#','#','#','#'],
+        ['#','.','.','.','.','#','#','#','#','#','#','#','#','.','.','.','.','&','P','T','#'],
+        ['#','.',',','.','.','#','.','.',',','.','.','Q','#','.','.',',','T','.',',','.','#'],
+        ['#','.',',','.','.','#','.','.',',','.','.','P','#','.','.',',','.','.',',','.','#'],
+        ['#','.',',','.','.','#','.','.',',','.','%','#','#','.','.',',','.','.',',','.','#'],
+        ['#','.',',','.','.','.','.','.','#','#','#','#','.','.',',',',',',',',','T',',','+'],
+        ['#','.',',','.','.','.','.','.','.',',','.','.','.','.','.','.',',','.','.','.','#'],
+        ['#','.',',','.','.','.','.','.','.',',','.','.','.','.',',','.',',','.','.','.','#'],
+        ['#','.',',','.','.','.','.','.','.',',','.','.','.','$','.','.',',','.','.','.','#'],
+        ['#','$','P','.','.','.','.','.','.',',','.','.','.','P','.','.',',','.',',','.','#'],
+        ['#','.',',','.','.','.','.','.','.',',','.','.','.','.','.','.',',','.','#','#','#','#','#','#','#','#','#'],
+        ['#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'],
+    ],
+    NPC: ["light green","black","dark blue","purple"],
+    Wall: 'dark green',
+    floorDots: 'lime green',
+    floorSpots: 'dark brown'
+};
+MapBox.push(map0);
+MapBox.push(map1);
 
 console.log('server dependencies loaded...');
 
--- static/client.js
+++ static/client.js
@@ -15,6 +15,51 @@ chat.addEventListener('submit', e => {
     chatInput.value = '';
 });
 
+const login = document.querySelector('.login-form');
+const username = document.querySelector('#name');
+const passphrase = document.querySelector('#passphrase');
+
+login.addEventListener('submit', e => {
+    e.preventDefault();
+    socket.emit('login', {name: username, phrase: passphrase} );
+});
+
+function draw(mapObj){
+    let canvas = document.getElementById('game');
+    let ctx = canvas.getContext('2d');
+    let tile = 32;
+    let xpos = 1, ypos = 1;
+    let countP = 0;
+    for (let i = 0; i < map.length; i++){
+        for (let j = 0; j < map.length; j++){
+            if (mapObj.map[i][j]==="#"){
+                ctx.fillStyle = mapObj.wallColor1;
+                ctx.fillText('#',(xpos*(j)*tile)+1, (ypos*(j)*tile)+1);
+            }
+            if (mapObj.map[i][j]==="."){
+                ctx.fillStyle = map.floorDots;
+                ctx.fillText('.',(xpos*(j)*tile)+1, (ypos*(j)*tile)+1);
+            }
+            if (mapObj.map[i][j]===","){
+                ctx.fillStyle = map.floorSpots;
+                ctx.fillText('.',(xpos*(j)*tile)+1, (ypos*(j)*tile)+1);
+            }
+            if (mapObj.map[i][j]==="P"){
+                ctx.fillStyle = map.NPC[countP];
+                ctx.fillText('P',(xpos*(j)*tile)+1, (ypos*(j)*tile)+1);
+            }
+            if (mapObj.map[i][j]==="+"){
+                ctx.fillStyle = "light brown";
+                ctx.fillText('+', (xpos*j*tile)+1,(ypos*j*tile)+1);
+            }
+            if (mapObj.map[i][j]==="&"){
+                ctx.fillStyle = "yellow";
+                ctx.fillText('&', (xpos*j*tile)+1,(ypos*j*tile)+1);
+            }
+        }
+    }
+}
+
 function render(message, id) {
     const output = document.createElement('p');
     if (id === socket.id) {
