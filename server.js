98e51bb3bd2beb9e2b10be4eefa08ece326f99e7
Daniel Rogahn <draquix@yahoo.com>
2022-03-15 18:21:55 -0500

    moving ahead with modular build for more asynch simulation

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const maps = require('./lib/maps');
 
 //Global Variablies
 let SOCKET_LIST = {};
-let PLAYER_LIST = {};
+var PLAYER_LIST = {};
 
 //Socket.io handling for client/server communications
 io.on('connection', socket => {
@@ -25,32 +25,88 @@ io.on('connection', socket => {
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
     socket.emit('handshaking', {id:socket.id,maps});
+    socket.on('chat', data => {
+        io.emit('msg', data);
+    });
 
     socket.on('login', user => {
         console.log('attempting login of: ',user);
+
         var player = {};
-        db.player.find({name:user.name}, (err,res) =>{
+        // db.player.find({name:user.name}, (err,res) =>{
             //console.log(err);
-            if(res.length>0){
-               if(res[0].passphrase==user.phrase){
-                    console.log('successfull login');
-                    player = pc.loginPlayer(user.name);
-               } else {
-                   console.log('wrong password');
-                   socket.emit('alert',{msg:"That name is taken but you have the wrong passphrase."});
-               }
-            }
+            // if(res.length>0){
+            //    if(res[0].passphrase==user.phrase){
+            //         console.log('successfull login');
+            //         player = pc.loginPlayer(user.name);
+            //    } else if (res[0].passphrase!=user.phrase) {
+            //        console.log('wrong password');
+            //        socket.emit('alert',{msg:"That name is taken but you have the wrong passphrase."});
+            //    }
+            // } else {
             console.log('new user');
+            let tell = "---<"+user.name+">-- "+"has just logged in!";
+            socket.emit('msg',{msg:tell});
             player = new pc.Player(user.name,user.phrase,socket.id);
             player.init();
-            pc.savePlayer(player);
+           // pc.savePlayer(player);
             //console.log(player);
-            console.log(player);
-            socket.emit('player update',player);
+            // }
+            // console.log(player);
 
+            PLAYER_LIST[socket.id] = player;
+            socket.emit('player update',player);
+            // console.log('Player list obj:',PLAYER_LIST[socket.id].backpack);
         });
+    socket.on('equip', data => {
+        var item = PLAYER_LIST[socket.id].backpack[data.index];
+        console.log('trying to equip: ',item);
+        if(item.type==="tool"){
+            PLAYER_LIST[socket.id].gear.tool.push(item);
+        }
+        PLAYER_LIST[socket.id].backpack.slice(data.index);
+
+        socket.emit('player update', PLAYER_LIST[socket.id]);
     });
+    socket.on('key press', data => {
+        console.log('Key fired: ',data);
+        var player = PLAYER_LIST[data.id];
+        if(data.target===","||data.target==="."||data.target==="+"||data.target===";"){
+            player.move(data.inputDir);
+        } else {
+            collision(data.id,player.xpos,player.ypos,data.target);
+        }
+    });
+
 });
+//Collision for map interaction
+function collision(id,x,y,targ){
+    if(targ==="1"||targ==="2"||targ==="3"||targ==="4"||targ==="5"||targ==="6"){
+        let poi = maps.PoIcheck(targ);
+        socket = SOCKET_LIST[id];
+        socket.emit('poi',{str:poi});
+    }
+    console.log('collision target: ',targ);
+}
+//Async runtime for live gameplay
+let ticker = 0;
+setInterval( function () {
+    ticker++;
+    if(ticker % 20===0){
+        console.log(`Running steady for ${ticker} game ticks.`);
+    }
+    let pack = [];
+    for(i in PLAYER_LIST){
+        pack.push({
+            xpos:PLAYER_LIST[i].xpos,
+            ypos:PLAYER_LIST[i].ypos,
+            id:PLAYER_LIST[i].id       
+         });
+        let socket = SOCKET_LIST[i];
+        socket.emit('Tick',pack);
+    }
+},200);
+
 
 //With all the files loaded, the below statement causes the server to boot up and listen for client connect
 
--- static/client.js
+++ static/client.js
@@ -5,7 +5,7 @@ const socket = io();
 const username = document.querySelector('#name');
 const passphrase = document.querySelector('#passphrase');
 const login = document.querySelector('.login-form');
-
+const entry = document.querySelector('#chatbox');
 //Event listener for login form submit
 login.addEventListener('submit', e => {
     e.preventDefault();
@@ -21,29 +21,40 @@ let ctx = canvas.getContext('2d');
 const love = 42;
 
 //global variables
-var localId;
+var localId=0;
 var maps =[];
+
+//Message chatting feature handler
+entry.addEventListener('submit', e => {
+    e.preventDefault();
+        var send = player.name + ": " + chattext.value;
+    socket.emit('chat', {msg:send});
+    chattext.value = " ";
+});
 
 //Handshaking client server connection
 socket.on('handshaking', data => {
     console.log('handed up id: ', data.id);
     localId = data.id;
     maps = data.maps;
+    console.log(maps);
 });
 socket.on('alert', data => {
+    console.log('alerting ',data);
     alert(data.msg);
 });
 socket.on('msg', data => {
-    let post = document.createElement('p');
+    console.log('message coming, ',data);
+    let post = document.createElement('li');
     post.innerHTML = data.msg;
     msgs.appendChild(post);
 });
 socket.on('player update', data => {
     player = data;
-    console.log(data);
-    draw(maps[0]);
+    draw(maps.mapArr);
     charDisplay();
 });
+
 
 let post = document.createElement('p');
 post.innerText = "display loaded";
@@ -86,7 +97,6 @@ function draw(map){
             if (map[i][j]==="P"){
                 ctx.fillStyle = 'blue';
                 ctx.fillText('P',(xpos*(j)*tile)+1, (ypos*(i+1)*tile)+1);
-                countP++;
             }
             if (map[i][j]===";"){
                 ctx.fillStyle = 'brown';
@@ -156,17 +166,96 @@ function draw(map){
     }
 }
 function charDisplay(){
-    let character = document.querySelector('#char-display');
+    let character = document.querySelector('#display');
+    character.innerHTML = " ";
     let stats = document.createElement('p');
     stats.innerHTML = player.name + " Statistics <BR> ";
     stats.innerHTML += `Hitpoints: ${player.hp.lvl} / ${player.mHp}  |  Coins: ${player.coin} <BR>`;
     stats.innerHTML += `Strength: ${player.str} | Dexterity: ${player.agi} | Defense: ${player.def} <BR>`;
     stats.innerHTML += `Mining: ${player.mine} Xp/Tnl: ${player.mineXp} / ${player.mineTnl} | Forging: ${player.forge} Xp/Tnl: ${player.forgeXp} / ${player.forgeTnl} <BR>`;
-    stats.innerHTML += `Woodcutting: ${player.chop} Xp/Tnl: ${player.chopXp} / ${chopTnl} | Crafting: ${player.craft} Xp/Tnl: ${player.craftXp} / ${player.craftTnl} <BR>`;
+    stats.innerHTML += `Woodcutting: ${player.chop} Xp/Tnl: ${player.chopXp} / ${player.chopTnl} | Crafting: ${player.craft} Xp/Tnl: ${player.craftXp} / ${player.craftTnl} <BR>`;
     stats.innerHTML += `Fishing: ${player.fish} Xp/Tnl: ${player.fishXp} / ${player.fishTnl} | Cooking: ${player.cook} Xp/Tnl: ${player.cookXp} / ${player.cookTnl} <BR>`;
     stats.innerHTML += `Carrying ${player.kg} kgs out of possible ${player.maxKg}. <BR>`;
-    stats.innerHTML += `Player is currently ${player.doing}. <BR>`;
-    stats.innerHTML += `<a href="javascript:invDisplay();"> Show Inventory </a><BR>`;
-    stats.innerHTML += `<a href="javascript:equipDisplay();"> Show Equipment </a>`;
+    stats.innerHTML += `Player is currently doing ${player.doFlag}. <BR>`;
+    if(player.gear.tool.length>0){
+        stats.innerHTML += `Currently using ${player.gear.tool[0].name} <br>`
+    } else {
+        stats.innerHTML += `You are not currently using anything for a tool. <br>`;
+    }
+    stats.innerHTML += `<a href="javascript:equipDisplay();"> Show Equipment </a> <br>`;
+    stats.innerHTML += "Currently carrying in backpack: ";
     character.appendChild(stats);
+    item = document.createElement('P');
+    for(i in player.backpack){
+        item.innerHTML += `A ${player.backpack[i].name} weighing ${player.backpack[i].kg} kgs `
+        if(player.backpack[i].type==="tool"){
+            item.innerHTML += ` <a href="javascript:equip(${i});"> Equip </a>, `;
+        } else if(player.backpack[i].stackable===true){
+            item.innerHTML += ` <a href="javascript:stack(${i});"> Stack </a>,`;
+        } else {
+            item.innerHTML += `, `;
+        }
+    }
+    character.appendChild(item);
 }
+function equip(num){
+    let item = player.backpack[num];
+    player.backpack.slice(num,1);
+    player.gear.tool.push(item);
+    socket.emit('equip',{index:num});
+}
+
+function equipDisplay(){
+    action.innerHTML = "";
+    for (const key in player.gear){
+        let item = document.createElement('p');
+        if(player.gear[key].length)
+            item.innerHTML = `${key}: A ${player.gear[key]}`;
+        else
+            item.innerHTML = `${key}: nothing.`;
+        action.appendChild(item);
+    }
+}
+socket.on('Tick', data =>{
+    draw(maps.mapArr);
+    player.xpos = data[0].xpos;
+    player.ypos = data[0].ypos;
+    // console.log('tick data',data);
+    var map = maps.mapArr;
+    var space=map[player.ypos][player.xpos];
+     console.log(space, player.xpos, player.ypos);
+    ctx.fillStyle="white";
+    ctx.fillText("C",(player.xpos)*18,(player.ypos+1)*18);
+});
+socket.on('poi', poi => {
+    console.log(poi);
+    action.innerHTML = " ";
+    let post = document.createElement('p');
+    post.innerHTML = poi.str.msg;
+    action.appendChild(post); 
+})
+document.onkeydown = function(event){
+    player.doing = 'Nothing';
+    var map = maps.mapArr;
+    let space = map[player.ypos][player.xpos];
+    if(localId===0){
+        return;}else{
+    if(event.keyCode === 68){  //d
+        space = map[player.ypos][player.xpos+1];
+        socket.emit('key press',{inputDir:'right', target:space, state:true, id:localId});
+    }if (event.keyCode === 83){ //s
+        space = map[player.ypos+1][player.xpos];
+        player.doing = "Walking";
+        socket.emit('key press', {inputDir:'down', target:space, state:true, id:localId});
+    } if (event.keyCode === 65) { //a
+        player.doing = "Walking";
+        space = map[player.ypos][player.xpos-1];
+        socket.emit('key press', {inputDir:'left', target:space, state:true, id:localId});
+    } if (event.keyCode === 87){ //w
+        player.doing = "Walking";
+        space = map[player.ypos-1][player.xpos];
+        socket.emit('key press', {inputDir:'up', target:space, state:true, id:localId});
+    }
+    }
+}
+
